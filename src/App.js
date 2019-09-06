28b4eb24c403274b5c684b2b0179007111830b5c
unknown <mbmaurya.maurya@gmail.com>
2019-09-06 16:05:07 +0530

    Completed chapter 17 - List Rendering, 18 - Lists and Keys, 19 - Index as Key Anti-pattern and 20 - Styling and CSS Basics

--- src/App.js
+++ src/App.js
@@ -11,11 +11,21 @@ import ClassClick from './components/ClassClick'
 import EventBind from './components/EventBind'
 import ParentComponent from './components/ParentComponent';
 import UserGreeting from './components/UserGreeting';
+import NameList from './components/NameList';
+import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline';
+import './appStyles.css'
+import styles from './appStyles.module.css'
 
 function App() {
   return (
     <div className="App">
-      <UserGreeting />
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      <Inline />
+      {/* <Stylesheet primary={true} /> */}
+      {/* <NameList /> */}
+      {/* <UserGreeting /> */}
       {/* <ParentComponent /> */}
       {/* <EventBind /> */}
       {/* <FunctionClick />
